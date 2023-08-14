349c0db1fdd7e6c940b1d770109384eda639535a
vanhieu200 <phanhieu200327@gmail.com>
2023-08-14 18:56:16 +0700

    ss6 toan tu

---
+++ ss6_operator/thuchanh/toantu.js
@@ -0,0 +1,7 @@
+inputWidth = prompt("input width: ");
+inputHeight = prompt("input height: ");
+let width = parseInt(inputWidth);
+let heigth  = parseInt(inputHeight);
+let result = width * heigth;
+
+document.write("diện tích hình chữ nhật là "+result);
